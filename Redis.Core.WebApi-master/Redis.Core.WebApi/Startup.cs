39259cc512f9158426173df61df868a77a9a09f1
Tulika Chaudharie <tulikac@microsoft.com>
2024-07-18 21:48:43 +0530

    Adding redis sample

---
+++ Redis.Core.WebApi-master/Redis.Core.WebApi/Startup.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redis.Core.WebApi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //Add Redis
+            services.AddStackExchangeRedisCache(options=>
+            {
+                options.Configuration = Configuration.GetConnectionString("Redis");
+                options.InstanceName = "Redis.Core.WebApi_";
+            });
+
+            //Add Dependency 
+            services.AddScoped<IWeatherForecastService, WeatherForecastService>();
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            
+        }
+    }
+}
---
+++ Redis.Core.WebApi-master/Redis.Core.WebApi/WeatherForecastService.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Logging;
+using Redis.Core.WebApi.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Redis.Core.WebApi
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private readonly IDistributedCache _cache;
+        private readonly ILogger<WeatherForecastService> _logger;
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private const string WeatherData_Key = "Weather_Data";
+
+        public WeatherForecastService(IDistributedCache cache,
+            ILogger<WeatherForecastService> logger)
+        {
+            _cache = cache;
+            _logger = logger;
+        }
+        public async Task<IEnumerable<WeatherForecast>> GetWeatherForecasts()
+        {
+            var dataFromCache = await _cache.GetRecordAsync<IEnumerable<WeatherForecast>>(WeatherData_Key);
+            if (dataFromCache != null)
+            {
+                _logger.LogInformation("Data loaded from cache @"+DateTime.Now);
+                return dataFromCache;
+            }
+            else
+            {
+                _logger.LogInformation("Data loaded from db @"+DateTime.Now);
+                var dataFromDb = GetDataFromDB();
+                await _cache.SetRecordAsync<IEnumerable<WeatherForecast>>(dataFromDb, WeatherData_Key);
+                return dataFromDb;
+            }
+        }
+
+        public IEnumerable<WeatherForecast> GetDataFromDB()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
